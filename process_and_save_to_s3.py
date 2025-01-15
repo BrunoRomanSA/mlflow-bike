da83df342b2cfd2d0d0ec6cc4a8163745beecee6
BrunoRoman <brunoroman05@gmail.com>
2025-01-14 21:21:29 -0300

    add permission

--- process_and_save_to_s3.py
+++ process_and_save_to_s3.py
@@ -6,7 +6,7 @@ import os
 s3_client = boto3.client('s3', region_name='us-east-1')
 bucket_name = "feature-store-bruno-data-bucket-dev"
 
-print(os.environ("PKG_CONFIG_PATH"))
+print(os.getenv("AWS_ACCESS_KEY_ID"))
 
 # Carregar e tratar os dados
 data = pd.read_csv("data/train.csv")
