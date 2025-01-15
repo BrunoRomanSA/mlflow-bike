aa2615a57119d47557b8e075e5da821627f1dc2b
BrunoRoman <brunoroman05@gmail.com>
2025-01-14 21:29:43 -0300

    add environment

--- process_and_save_to_s3.py
+++ process_and_save_to_s3.py
@@ -4,7 +4,7 @@ import os
 
 # Configuração do S3
 s3_client = boto3.client('s3', region_name='us-east-1')
-bucket_name = f"feature-store-bruno-data-bucket-{os.getenv("ENVIRONMENT")}"
+bucket_name = f"feature-store-bruno-data-bucket-dev"
 
 # Carregar e tratar os dados
 data = pd.read_csv("data/train.csv")
@@ -26,6 +26,6 @@ s3_client.upload_file(
 )
 
 print(f"Dados tratados enviados para s3://{bucket_name}/processed/processed_data.parquet")
-
+print(os.getenv("ENVIRONMENT"))
 # Limpar o arquivo local
 os.remove(output_file)
