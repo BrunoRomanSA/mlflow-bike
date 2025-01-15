fcb8bd602f1440cb19d1072b7202368e108fd01f
BrunoRoman <brunoroman05@gmail.com>
2025-01-14 21:06:52 -0300

    deploy pipe data

--- infra/dev/terraform.tfvars
+++ infra/dev/terraform.tfvars
@@ -1,1 +1,3 @@
-name="feature-store-bruno-data-bucket-dev"
+bucket_data="feature-store-bruno-data-bucket-dev"
+role_name="S3AccessRole-dev"
+policy_name="S3AccessPolicy-dev"
--- infra/variables.tf
+++ infra/variables.tf
@@ -1,4 +1,14 @@
-variable "name" {
+variable "bucket_data" {
   description = "Name of bucket data"
+  type        = string
+}
+
+variable "role_name" {
+  description = "Name of access role"
+  type        = string
+}
+
+variable "policy_name" {
+  description = "Name of access role"
   type        = string
 }
